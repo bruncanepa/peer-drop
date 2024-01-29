42c36707372792a9b21fca90871c853b64631b8d
Bruno Canepa <8711973+bruncanepa@users.noreply.github.com>
2024-01-28 23:14:57 -0300

    Integrated ChakraUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { FC, lazy, Suspense } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 const Receiver = lazy(() => import("components/Receiver"));
 const Sender = lazy(() => import("components/Sender"));
@@ -7,25 +8,27 @@ const App: FC = () => {
   const sharedSessionIdParam = window.location.pathname.replace("/", "");
 
   return (
-    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-evenly",
-          width: "90%",
-          margin: "5%",
-        }}
-      >
-        <Suspense fallback={<div>Loading...</div>}>
-          {Boolean(sharedSessionIdParam) ? (
-            <Receiver sharedId={sharedSessionIdParam} />
-          ) : (
-            <Sender />
-          )}
-        </Suspense>
+    <ChakraProvider>
+      <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            width: "90%",
+            margin: "5%",
+          }}
+        >
+          <Suspense fallback={<div>Loading...</div>}>
+            {Boolean(sharedSessionIdParam) ? (
+              <Receiver sharedId={sharedSessionIdParam} />
+            ) : (
+              <Sender />
+            )}
+          </Suspense>
+        </div>
       </div>
-    </div>
+    </ChakraProvider>
   );
 };
 
